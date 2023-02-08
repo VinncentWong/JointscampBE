dfac48f14e90fe3fce1d9fa7678ed58a77a9f92e
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-08 20:43:30 +0700

    INITIAL COMMIT

---
+++ jointscamp/src/main/kotlin/centkarts/jointscamp/JointscampApplication.kt
@@ -0,0 +1,11 @@
+package centkarts.jointscamp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JointscampApplication
+
+fun main(args: Array<String>) {
+	runApplication<JointscampApplication>(*args)
+}
---
+++ jointscamp/src/test/kotlin/centkarts/jointscamp/JointscampApplicationTests.kt
@@ -0,0 +1,13 @@
+package centkarts.jointscamp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JointscampApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
