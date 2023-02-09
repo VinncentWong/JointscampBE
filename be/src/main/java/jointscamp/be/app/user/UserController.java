eff130131e3506cf840c75e174107eb1b65eca7b
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-09 16:02:31 +0700

    [ADD] user controller

---
+++ be/src/main/java/jointscamp/be/app/user/UserController.java
@@ -0,0 +1,39 @@
+package jointscamp.be.app.user;
+
+import jakarta.validation.Valid;
+import jointscamp.be.dto.user.LoginUserDto;
+import jointscamp.be.dto.user.RegisterUserDto;
+import jointscamp.be.exception.UserExistException;
+import jointscamp.be.exception.UserNotAuthenticatedException;
+import jointscamp.be.exception.UserNotFoundException;
+import jointscamp.be.util.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private final UserService service;
+
+    public UserController(UserService service){
+        this.service = service;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Response> createUser(@RequestBody @Valid RegisterUserDto dto) throws UserExistException {
+        return this.service.createUser(dto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> loginUser(@RequestBody @Valid LoginUserDto dto) throws UserNotFoundException, UserNotAuthenticatedException {
+        return this.service.loginUser(dto);
+    }
+}
