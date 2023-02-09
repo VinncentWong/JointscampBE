2d55fb6ede9f206cb5fa66795e33536e59bd5607
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-09 16:05:13 +0700

    [ADD] register user dto

---
+++ be/src/main/java/jointscamp/be/dto/user/RegisterUserDto.java
@@ -0,0 +1,36 @@
+package jointscamp.be.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jointscamp.be.entity.User;
+import jointscamp.be.util.BcryptUtil;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public record RegisterUserDto(
+        @NotNull(message = "email should not null")
+        @NotBlank(message = "email should not blank")
+        @Email(message = "should be an email")
+        String email,
+
+        @NotNull(message = "email should not null")
+        @NotBlank(message = "email should not blank")
+        @Length(min = 6, message = "password length should >= 6")
+        String password,
+
+        @NotNull(message = "email should not null")
+        @NotBlank(message = "email should not blank")
+        @Length(min = 6, message = "username length should >= 6")
+        String username
+) {
+    public User transformIntoUser(){
+        return User
+                .builder()
+                .email(this.email)
+                .password(BcryptUtil.bcrypt().encode(this.password))
+                .build();
+    }
+}
