46459f926482630697e3a6694ff4ebf0711f6579
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-09 16:05:42 +0700

    [ADD] like id

---
+++ be/src/main/java/jointscamp/be/entity/LikeId.java
@@ -0,0 +1,21 @@
+package jointscamp.be.entity;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Embeddable
+class LikeId implements Serializable {
+    private Long userId;
+    private UUID produkId;
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof LikeId id){
+            return id.produkId.compareTo(this.produkId) == 0 && (this.userId.equals(id.userId));
+        } else {
+            return false;
+        }
+    }
+}
