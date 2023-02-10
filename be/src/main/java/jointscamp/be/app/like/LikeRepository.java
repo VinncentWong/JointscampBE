bc3c3318966e6f2ffc7fce99a6a4ba509a664542
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-10 18:26:50 +0700

    [ADD] like repository

---
+++ be/src/main/java/jointscamp/be/app/like/LikeRepository.java
@@ -0,0 +1,24 @@
+package jointscamp.be.app.like;
+
+import jointscamp.be.entity.Like;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface LikeRepository extends CrudRepository<Like, Long> {
+
+    @Query(nativeQuery = true, value =
+            "SELECT COUNT(user_id) FROM likes GROUP BY produk_id HAVING produk_id=?1"
+    )
+    public Long getProdukLike(UUID idProduk);
+
+    @Query(nativeQuery = true, value = "DELETE FROM produk WHERE produk_id = ?1 AND user_id = ?2")
+    public void deleteProdukLike(UUID idProduk, Long idUser);
+
+    @Query(nativeQuery = true, value = "SELECT user_id FROM likes WHERE produk_id = ?1")
+    public List<Long> getUserLike(UUID produkId);
+}
