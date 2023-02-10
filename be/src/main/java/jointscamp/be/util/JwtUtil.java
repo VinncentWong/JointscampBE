30d8e535ce21ce402d80675cd3d0ef9d20a09382
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-10 18:26:10 +0700

    [CHANGE] claims instance

--- be/src/main/java/jointscamp/be/util/JwtUtil.java
+++ be/src/main/java/jointscamp/be/util/JwtUtil.java
@@ -7,21 +7,22 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
 public class JwtUtil {
 
-    @Value("secret_key")
+    @Value("${SECRET_KEY}")
     private String secret_key;
 
     public String generateToken(Extractable data){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", data.getId());
+        claims.put("username", data.getUsername());
+        claims.put("createdAt", data.getCreatedAt());
         return Jwts.builder()
-                .addClaims(Map.of(
-                        "id", data.getId(),
-                        "username", data.getUsername(),
-                        "createdAt", data.getCreatedAt()
-                ))
+                .addClaims(claims)
                 .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret_key)), SignatureAlgorithm.HS256)
                 .compact();
     }
