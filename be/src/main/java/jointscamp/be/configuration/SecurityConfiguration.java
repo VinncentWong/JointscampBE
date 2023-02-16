cb9a3ae7748509a3ba3de909fa143fc61baef925
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-16 15:32:39 +0700

    [ADD] request authorization

--- be/src/main/java/jointscamp/be/configuration/SecurityConfiguration.java
+++ be/src/main/java/jointscamp/be/configuration/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package jointscamp.be.configuration;
 
+import jointscamp.be.security.filter.JwtFilter;
+import jointscamp.be.security.manager.JwtManager;
+import jointscamp.be.security.provider.JwtProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
@@ -13,6 +17,29 @@ public class SecurityConfiguration {
         return http
                 .csrf().disable()
                 .httpBasic().disable()
+                .exceptionHandling()
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/produk/**").authenticated()
+                .requestMatchers("/user/create", "/user/login", "/user/get/{id}").permitAll()
+                .requestMatchers("/user/**").authenticated()
+                .requestMatchers("/like/**").authenticated()
+                .and()
+                .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public JwtProvider jwtProvider(){
+        return new JwtProvider();
+    }
+
+    @Bean
+    public JwtManager jwtManager(){
+        return new JwtManager(jwtProvider());
+    }
+    @Bean
+    public JwtFilter jwtFilter(){
+        return new JwtFilter(jwtManager());
     }
 }
