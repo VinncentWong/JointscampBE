9a03d81c24ead8910816005b709aed8f752b3b5d
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-20 09:48:20 +0700

    INITIAL COMMIT

---
+++ fe/jointscamp/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <div>
+
+      </div>
+    </ChakraProvider>
+  );
+}
+
+export default App;
