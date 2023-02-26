bf80f88900980f37d564b5702cb26da5e2b8f745
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2023-02-26 15:33:20 +0700

    [CHANGE] query to use JPQL and use join fetch

--- be/src/main/java/jointscamp/be/app/user/UserRepository.java
+++ be/src/main/java/jointscamp/be/app/user/UserRepository.java
@@ -1,15 +1,16 @@
 package jointscamp.be.app.user;
 
 import jointscamp.be.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE email = ?1")
-    public Optional<User> getUserByEmail(String email);
+    @Query("SELECT u FROM users u left join fetch u.produks WHERE u.email = :email")
+    public Optional<User> getUserByEmail(@Param("email") String email);
 }
